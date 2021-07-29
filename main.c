56221ad6a62d2b60d578c506c8c36967d7a1a36d
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-07-29 18:09:42 -0300

    Add `count_zeroes`

--- main.c
+++ main.c
@@ -15,6 +15,17 @@ bool get_bit_at(uint8_t byte, uint8_t idx) {
     return ((byte >> (idx))  & 0x01);
 }
 
+//! Will count how many bits are unset in `byte`
+uint8_t count_zeroes(uint8_t byte) {
+    uint8_t count = 0;
+    for (uint8_t i = 0; i < 8; i++) {
+        if (!get_bit_at(byte, i)) {
+            count++;
+        }
+    }
+    return count;
+}
+
 //! Converts an integer to binary
 unsigned int_to_binary(unsigned k) {
     if (k == 0) { return 0; }
@@ -23,8 +34,8 @@ unsigned int_to_binary(unsigned k) {
 }
 
 //! Makes sure any functions relating to binary logic work as they should
-void sanity_check_binary_logic() {
-    fprintf(stderr, "[self-test] Sanity checking binary manipulation logic.\n");
+void sanity_check_get_bit_at() {
+    fprintf(stderr, "[self-test] Sanity checking get_bit_at\n");
     // 25 is 0b11001
     uint8_t twenty_five = 25;
 
@@ -38,8 +49,38 @@ void sanity_check_binary_logic() {
     fprintf(stderr, "[self-test] OK\n");
 }
 
+void sanity_check_count_zeroes() {
+    fprintf(stderr, "[self-test] Sanity checking count_zeroes\n");
+    // 65 is 0b10000010
+    // 6 zeroes
+    uint8_t sixty_five = 65;
+
+    // 0 is 0b0
+    // 8 zeroes
+    uint8_t min_byte = 0;
+
+    // 255 is 0b11111111
+    // No zeroes
+    uint8_t max_byte = 255;
+
+    assert(
+        count_zeroes(sixty_five) == 6
+    );
+
+    assert(
+        count_zeroes(min_byte) == 8
+    );
+
+    assert(
+        count_zeroes(max_byte) == 0
+    );
+
+    fprintf(stderr, "[self-test] OK\n");
+}
+
 int main()
 {
-    sanity_check_binary_logic();
+    sanity_check_get_bit_at();
+    sanity_check_count_zeroes();
     return 0;
 }
