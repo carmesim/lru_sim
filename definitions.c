af95f226aea50257fc7eaa4184b54266bbad1806
Frederico J. R. Pelogia <fredpelogia@outlook.com>
2021-07-29 22:25:39 -0300

    Update `reference_page` logic
    It isn't finished yet

--- definitions.c
+++ definitions.c
@@ -15,11 +15,53 @@ int reference_page(uint8_t addr){
 
     if(pte.is_mapped){
         // reference page
+
+        // assuming it's on the real memory
         real_memory[pte.real_addr].page.referenced_counter++;
+
     }else{
-        // page miss 
+        // page fault
+        uint8_t real_addr = get_free_real_address();
+        if(real_addr == -1){//real memory is full
+            // page miss
+            
+            // generate addr form N_SLOTS_RM to N_SLOTS_VM
+            uint8_t swap_addr = rand()%N_SLOTS_VM + N_SLOTS_RM;
 
+            // find lru page and puts it into swap
+            uint8_t lib_adrr;
+            swap[swap_addr].page = lru_page(&lib_adrr);
+
+            // the freed address will recieve the new page
+            real_memory[lib_adrr].page.referenced_counter++;
+            page_t new_page = malloc(sizeof(page_t));
+            new_page.referenced_counter = 1;
+            real_memory[lib_adrr].page = new_page;
+
+            pte.real_addr = lib_addr;
+        }else{
+
+        }
+
+        if(addr >= N_SLOTS_RM){ 
+            // page miss
+            uint8_t lib_adrr;
+            swap[pte.real_addr].page = lru_page(&lib_adrr);
+            real_memory[lib_adrr].page.referenced_counter++;
+            page_t new_page = malloc(sizeof(page_t));
+            new_page.referenced_counter = 1;
+            real_memory[lib_adrr].page = new_page;
+        }else{
+            uint8_t real_addr = rand()%N_SLOTS_RM;
+            page_t new_page = malloc(sizeof(page_t));
+            new_page.referenced_counter = 1;
+            real_memory[real_addr].page = new_page;
+        }
     }
-
     return 0;
+}
+
+
+page_t lru_page(uint8_t *lib_addr){
+    // [TODO]
 }
--- definitions.h
+++ definitions.h
@@ -8,7 +8,6 @@
 #define N_SLOTS_SWAP 8 // |SW| >= |VM| - |RM|
 
 typedef struct{
-    bool R;//bit referenciada
     uint8_t referenced_counter;
 }page_t;
 
@@ -22,13 +21,19 @@ extern mem_slot_t swap[N_SLOTS_SWAP];
 
 typedef struct{
     bool is_mapped;
+    bool R;// referenced bit
     uint8_t real_addr; // between 0 and N_SLOTS_RM - 1
 }page_table_entry_t;
 
 // process table stores the mapping between VM and RM
 extern page_table_entry_t page_table[N_SLOTS_VM];
 
-int reference_page(uint8_t addr);
+int reference_page(uint8_t lib_addr);
+
+// returns the last recently used page 
+// and changes lib_addr to the freed address
+// on the real memory
+page_t lru_page(uint8_t *lib_addr);
 
 
 
