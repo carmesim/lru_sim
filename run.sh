d6e39d644df4eaf1905fea6669918ed96bcf0de6
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-08-06 11:21:04 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+DIR=$(dirname "$BASH_SOURCE")
+cd "$DIR"
 gcc -c -pipe -O3 -Wall -Wextra -fPIC  -I. -o binutils.o binutils.c
 gcc -c -pipe -O3 -Wall -Wextra -fPIC  -I. -o definitions.o definitions.c
 gcc -c -pipe -O3 -Wall -Wextra -fPIC  -I. -o main.o main.c
-g++ -Wl,-O1 -o lru_sim binutils.o definitions.o main.o
+gcc -Wl,-O1 -o lru_sim binutils.o definitions.o main.o
+rm *.o
 ./lru_sim $1
